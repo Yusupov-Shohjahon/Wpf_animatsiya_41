28a0a88acaf88b085f803beda6e8d96602b02c8f
JAHONGIR_PC <odilovjahongir1993@gmail.com>
2025-04-14 09:42:37 +0500

    3

--- Wpf_animatsiya_41/MainWindow.xaml.cs
+++ Wpf_animatsiya_41/MainWindow.xaml.cs
@@ -81,7 +81,7 @@ namespace Wpf_animatsiya_41
 
             var colorAnim1 = new ColorAnimation
             {
-                From = Colors.Red,
+                From = Colors.AliceBlue,
                 To = Colors.Orange,
                 Duration = TimeSpan.FromSeconds(2),
                 AutoReverse = true,
